08e689f48ebbd50c1a693f2cb544d20dd850e259
Mohammad <mohammadfallah.w@gmail.com>
2024-09-03 15:39:18 +0330

    fix layout bugs

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,14 @@ const App = () => {
   const [selectedCategory, setSelectedCategory] = useState<string>("");
 
   return (
-    <div className="container mx-auto px-4 sm:px-6 grid grid-cols-6">
-      <div className="col-span-1 hidden sm:block h-screen">
+    <div className="grid grid-cols-6 gap-x-28 px-5">
+      <div className="col-span-1 hidden h-screen sm:block">
         <SideBar
           selectedCategory={selectedCategory}
           onSelectCategory={(category) => setSelectedCategory(category)}
         />
       </div>
-      <div className="col-span-6 sm:col-span-5 sm:pl-24">
+      <div className="col-span-6 sm:col-span-5 sm:border-l sm:pl-12">
         <TaskView selectedCategory={selectedCategory} />
       </div>
     </div>
