7c2f1a2da374c88ddaa35ffb06ee459f433c9132
Mohammad <mohammadfallah.w@gmail.com>
2024-09-03 14:09:42 +0330

    implement main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="container mx-auto px-4 sm:px-6 grid grid-cols-6">
+      <div className="col-span-1 border hidden sm:block">Side</div>
+      <div className="col-span-6 sm:col-span-5 border">Main</div>
+    </div>
+  );
 };
 
 export default App;
